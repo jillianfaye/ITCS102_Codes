0b5c7da9b7ffd31ba4465a117976b4368f03229c
jillianfaye <ducayjillianfaye@gmail.com>
2026-09-01 19:36:48 +0800

    Add files via upload
    code challenge3

---
+++ third.py
@@ -0,0 +1,11 @@
+#first step to dynamic content is through INPUTS (User Input)
+
+name = input("What is your name? ")
+print("Hi", name, "How are you doing?")
+
+yes_no = input("Did you eat? ")
+
+if yes_no == "yes":
+    print("Good to know")
+else:
+    print("Oh no!")
