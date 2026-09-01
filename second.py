1308ed894c29bbd7906b8a6863b638bc619eaee7
jillianfaye <ducayjillianfaye@gmail.com>
2026-09-01 19:07:13 +0800

    Add files via upload
    code challenge 2

---
+++ second.py
@@ -0,0 +1,38 @@
+money = 6722
+
+a = 1000
+b = 500
+c = 200
+d = 100
+e = 50
+f = 20
+g = 10
+h = 5
+i = 1
+
+print("1000 =", money // a)
+money = money % a
+
+print("500 =", money // b)
+money = money % b
+
+print("200 =", money // c)
+money = money % c
+
+print("100 =", money // d)
+money = money % d
+
+print("50 =", money // e)
+money = money % e
+
+print("20 =", money // f)
+money = money % f
+
+print("10 =", money // g)
+money = money % g
+
+print("5 =", money // h)
+money = money % h
+
+print("1 =", money // i)
+money = money % i
