957cc79b16b34cc2165a8b9d3493970ed6cd5487
jillianfaye <ducayjillianfaye@gmail.com>
2026-09-03 10:57:13 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,29 @@
+#operators
+#Aritmethic Operators
+# + - Addition 
+# - - Substaction
+# * - Multiplication
+# / - Division
+# // - Floor Division 
+# ** - Exponentation
+# & - Modulus
+
+a = 29
+b = 22
+
+sum = a + b 
+diff = a - b
+product = a * b
+division = a / b
+Floordivision = a // b 
+Exponentation = a ** b
+Modulus = a & b
+
+
+print("The sum of ",a," and ",b," is",sum)
+print("The difference of ",a," and ",b," is",diff)
+print("The product of ",a," and ",b," is",product)
+print("The division of ",a," and ",b," is",division)
+print("The floor division of ",a," and ",b," is",a//b)
+print("Exponentation of ",a," and ",b," is",Exponentation)
+print("Modulus of ",a," and ",b," is",Modulus)
