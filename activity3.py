b15405939eb3fa506038d5f11257f41bb747f85e
jillianfaye <ducayjillianfaye@gmail.com>
2026-09-03 10:56:31 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,19 @@
+#integer variable
+age =18
+
+#float variable
+price = 67.67
+
+#string variable
+name = "jillian fae"
+password = "electricfan_qopatotok"
+
+#boolean
+lovescats = True
+istall = True
+
+# Print
+
+print("Hi, my name is,Lian")
+print("and right neow my age is 18")
+print("suntukan tau right neow")
