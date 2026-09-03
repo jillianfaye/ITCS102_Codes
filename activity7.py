9df854ce5bda1011f3a15732cc6a621b1b3e2e2f
jillianfaye <ducayjillianfaye@gmail.com>
2026-09-03 10:57:51 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,20 @@
+#assignment operator 
+
+total = 0 
+
+n1 =eval(input("Input a number -> "))
+total += n1
+
+n2 =eval(input("Input a number -> "))
+total -= n2
+
+n3 =eval(input("Input a number -> "))
+total *= n3
+
+n4 =eval(input("Input a number -> "))
+total /= n4
+
+print("Total is -> ", total)
+
+
+
