698d3eb112c9d53cd052ef44814b700e9700befd
jillianfaye <ducayjillianfaye@gmail.com>
2026-09-03 10:56:48 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,14 @@
+#escape sequence
+#a way to modify inline string
+# \n - new line
+# \t - tab space
+# \r - carriage return
+# \\ - insert slash
+# \" - insert quoation
+# \b - back space
+
+emo_phrase = "Good things takes time.\n nyehehehwahahaha"
+
+second_p = "Cats makes people happy.\n \"meow meow\". Imissubby"
+print (emo_phrase)
+print(second_p)
