51a75f5c97d45d4927a6a55e3c2fed90d1073b0f
jillianfaye <ducayjillianfaye@gmail.com>
2026-09-08 11:14:42 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,16 @@
+x = 10
+y = 5
+
+username = 'Meowycake'
+password = 'electricfan_nibatman'
+
+print( x >= y and username == 'Meowycake' and password == 'electricfan_nibatman')
+
+print(not( x >= y and username == 'Meowycake' and password == 'electricfan_nibatman'))
+
+print( x >= y and username == 'Meowycake' or password == 'electricfan_nibatman')
+
+print( x >= y and username == 'Meowymeow' and password == 'electricfan_nibatman')
+
+
+
