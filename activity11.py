90df0e7b67c9d094a3f3e4a106d98843e378d17b
jillianfaye <ducayjillianfaye@gmail.com>
2026-09-08 11:14:58 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,10 @@
+username = 'suntukan_tau'
+password = 'right_neow'
+
+u = input("Enter Username ----> ")
+p = input("Enter Password ----> ")
+
+if username == u and password == p :
+	print("ACCESS GRANTED")
+else:
+	print("ACCESS DENIED")
