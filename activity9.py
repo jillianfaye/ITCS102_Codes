e0435d3a77be7c4e4a0e3d69d4b285a93099144a
jillianfaye <ducayjillianfaye@gmail.com>
2026-09-08 11:14:28 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,21 @@
+#relational operator demo
+
+x = 6
+y = 7
+
+user1 = "sample"
+user2 = "sample"
+
+#1
+print( x > y)
+#2
+print( x < y)
+#3
+print( x >= y)
+#4
+print( x <= y)
+#5
+print( x == y)
+print( user1 == user2)
+#6
+print( user1 != user2)
