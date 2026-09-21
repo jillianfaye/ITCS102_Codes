d6e72f04c59c683f84a5a32bdb07e3d18e8164f5
jillianfaye <ducayjillianfaye@gmail.com>
2026-09-21 22:16:16 +0800

    Add files via upload

---
+++ activity14.py
@@ -0,0 +1,43 @@
+# age (integer)
+# is_employed (boolean)
+# credit_score (integer)
+# annual_income (float)
+#has_collateral (boolean)
+
+age = int(input("Enter your age -->  "))
+is_employed = input("Are you currently employed? (True / False)  -->  ") == "True"
+credit_score = float(input("Credit Score -->  "))
+annual_income = float(input("What is your annual salary? -->"))
+has_collateral = input("Do you have any collateral?  (True / False) -->") == "True"
+
+
+if age >= 21 and is_employed == True:
+    print("Accepted Baseline Criteria")
+    if credit_score >=750:
+        print("You have a high credit score")
+        if annual_income >=1000000:
+                base_rate = 4.5
+                print("Your base rate is", base_rate)
+        else:
+            base_rate = 5.0
+            print("Your base rate is", base_rate)
+    elif credit_score >=600 and credit_score <750:
+        print('Your credit score is less than 750')
+
+        if has_collateral == True:
+               print("You have collateral")
+               base_rate = 7.0
+               print("Your base rate is", base_rate)
+
+        elif annual_income <=40000:
+               print("Low annual income")
+               base_rate = 9.5
+               print("Your base rate is", base_rate)
+
+        else:
+            base_rate = 8.0
+            print("Your base rate is", base_rate)
+    elif credit_score < 600:
+        print("Rejected: Credit Score too low")
+else:
+    print("Rejected: Fails baseline criteria")
